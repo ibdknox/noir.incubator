d08338fcab5c85f09e3aee44c57489cdfc223adc
Joshua Smith <kognate@gmail.com>
2011-12-23 22:18:48 -0500

    corrected cut-and-paste error that broke the code.

--- src/noir.incubator/middleware.clj
+++ src/noir.incubator/middleware.clj
@@ -6,16 +6,16 @@
    and total time spent processing the request."
   (fn [{:keys [request-method uri] :as req}]
     (let [start  (System/currentTimeMillis)
-          resp   (handler req)
-          status (:status resp)
-          finish (System/currentTimeMillis)
-          total  (- finish start)]
-          (formatter request-method uri total)
-          resp)))
+	  resp   (handler req)
+	  status (:status resp)
+	  finish (System/currentTimeMillis)
+	  total  (- finish start)]
+      (formatter request-method status uri total)
+      resp)))
 
-(defn- logformatter [reqmeth uri totaltime]
+(defn- logformatter [reqmeth status uri totaltime]
   "Basic logformatter for loging middleware."
-  (let [line (format "[%s] %s [Status: %s] %s (%dms)" (java.util.Date.)  reqmeth uri totaltime)]
+  (let [line (format "[%s] %s [Status: %s] %s (%dms)" (java.util.Date.)  reqmeth status uri totaltime)]
     (locking System/out (println line))))
 
 (defn wrap-request-loging [handler]
